c8462b376133a45e413e70a4ecba1aa2e17da3bc
wuzhenting <wuzt@kh-super.com>
2026-06-20 23:22:27 +0800

    ready

--- node_modules/es-toolkit/dist/compat/array/size.mjs
+++
@@ -1,9 +0,0 @@
-import { isNil } from "../../predicate/isNil.mjs";
-//#region src/compat/array/size.ts
-function size(target) {
-	if (isNil(target)) return 0;
-	if (target instanceof Map || target instanceof Set) return target.size;
-	return Object.keys(target).length;
-}
-//#endregion
-export { size };
