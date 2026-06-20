2d625c061edf1915c523711b9459cc75fe8f948a
wuzhenting <wuzt@kh-super.com>
2026-06-20 23:22:27 +0800

    ready

--- node_modules/es-toolkit/dist/_internal/compareValues.js
+++
@@ -1,8 +0,0 @@
-//#region src/_internal/compareValues.ts
-function compareValues(a, b, order) {
-	if (a < b) return order === "asc" ? -1 : 1;
-	if (a > b) return order === "asc" ? 1 : -1;
-	return 0;
-}
-//#endregion
-exports.compareValues = compareValues;
